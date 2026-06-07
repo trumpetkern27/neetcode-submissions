912b2c10df32eba5c48ae26cdde64b91d0804131
trumpetkern27 <trumpetkern@gmail.com>
2026-06-07 15:35:08 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        temp = []
+        m = self.stack[-1]
+        while len(self.stack):
+            m = min(m, self.stack[-1])
+            temp.append(self.stack.pop())
+        while len(temp):
+            self.stack.append(temp.pop())
+        return m
